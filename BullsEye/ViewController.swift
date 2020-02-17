e16c0798f154c339b04b1f84ee4f5d9fa8c35b70
Jelle Zwart <jelle.zwart@icloud.com>
2020-02-17 15:46:30 +0100

    Setup initial project

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert() {
+    
+    let alert = UIAlertController(title: "Hello world!", message: "This is my first app", preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "Awsome", style: .default, handler: nil)
+    
+    alert.addAction(action)
+    
+    present(alert,animated: true, completion: nil)
+    }
 
 }
 
