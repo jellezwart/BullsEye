c063108b885f19df04e4f81929e1fe87ffc7321a
Jelle Zwart <jelle.zwart@icloud.com>
2020-02-18 14:00:18 +0100

    Fine tuning the code

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,37 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 0
+    var currentValue = 0
     var targetValue = 0
+    var score = 0
+    var round = 0
     
     @IBOutlet weak var slider: UISlider!
-    @IBOutlet var targetLable: UILabel!
+    @IBOutlet weak var targetLable: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startNewRound()
+        startNewGame()
+        updateLabels()
     }
     
     @IBAction func showAlert() {
         
-        let message = "The value of the slider is now \(currentValue)" +
-            "\n The target value is \(targetValue)"
+        let difference = abs(targetValue - currentValue)
+        
+        var points = 100 - difference
+
+        score = score + points
+        
+        let title: String
+          if difference == 0 {
+            title = "Kei-goed Lenne!"
+            points = points + 100
+          } else if difference < 5 {
+            title = "Oh, jammer... en zo dicht bij..."
+            points = points + 50
+          } else if difference < 10 {
+            title = "Bwa, niet slecht."
+          } else {
+            title = "Pfff, meende da nu?"
+          }
+        
+        let message = "Je plaatste de slider op \(currentValue)" +
+            "\n Het verschil is \(difference)" +
+            "\n Je scoorde \(points) punten"
          
     
-        let alert = UIAlertController(title: "Hello world!", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
     
-        let action = UIAlertAction(title: "Awsome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: {_ in self.startNewRound()})
     
         alert.addAction(action)
     
         present(alert,animated: true, completion: nil)
         
-        startNewRound()
     }
     
     func startNewRound(){
+        round = round + 1
         targetValue = Int.random(in: 1...100)
         currentValue = 50
         slider.value = Float(currentValue)
@@ -48,8 +73,17 @@ class ViewController: UIViewController {
         updateLabels()
     }
     
+    
+    @IBAction func startNewGame() {
+        round = 0
+        score = 0
+        startNewRound()
+    }
+    
     func updateLabels(){
         targetLable.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     
     
